65e01c66bb3ed35ce2235f226ae9fca1ddb77140
Chandima Silva <chandimasilva1@gmail.com>
2018-09-23 16:35:46 +0530

    root module done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatListModule} from '@angular/material/list';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,
+    MatToolbarModule,FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
